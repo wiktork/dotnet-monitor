b8e0fa41d95784cd51d50bb4e4880b6edbb2afd5
Wiktor Kopec <wiktork@microsoft.com>
2021-03-31 17:14:58 -0700

    cr feedback

---
+++ src/Tests/Microsoft.Diagnostics.Monitoring.ConfigurationSchema/SchemaGenerator.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Diagnostics.Monitoring.UnitTests.Options;
+using NJsonSchema;
+using NJsonSchema.Generation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Diagnostics.Monitoring.ConfigurationSchema
+{
+    internal sealed class SchemaGenerator
+    {
+        public string GenerateSchema()
+        {
+            var settings = new JsonSchemaGeneratorSettings();
+
+            JsonSchema schema = JsonSchema.FromType<RootOptions>(settings);
+            schema.Id = @"http://www.github.com/dotnet/dotnet-monitor";
+            schema.Title = "DotnetMonitorConfiguration";
+
+            //HACK Even though Properties is defined as JsonDataExtension, it still emits a property
+            //called 'Properties' into the schema, instead of just specifying additionalProperties.
+            //There is likely a more elegant way to fix this.
+            schema.Definitions[nameof(EgressProvider)].Properties.Remove(nameof(EgressProvider.Properties));
+
+            string schemaPayload = schema.ToJson();
+            return schemaPayload;
+        }
+    }
+}
