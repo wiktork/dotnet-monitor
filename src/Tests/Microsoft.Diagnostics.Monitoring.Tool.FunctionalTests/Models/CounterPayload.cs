a0b7273c81c0c3b6870f24692b55738f368834ff
Wiktor Kopec <wiktork@microsoft.com>
2021-08-27 16:50:22 -0700

    PR Feedback

---
+++ src/Tests/Microsoft.Diagnostics.Monitoring.Tool.FunctionalTests/Models/CounterPayload.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace Microsoft.Diagnostics.Monitoring.Tool.FunctionalTests.Models
+{
+    internal class CounterPayload
+    {
+        [JsonPropertyName("provider")]
+        public string Provider { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+    }
+}
