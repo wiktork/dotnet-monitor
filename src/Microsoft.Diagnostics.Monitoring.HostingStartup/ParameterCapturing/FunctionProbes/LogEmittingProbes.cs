b25118ae41177d71b992b299aed4e7798e021a41
Wiktor Kopec <wiktork@microsoft.com>
2023-07-25 16:53:36 -0700

    Background logs prototype

--- src/Microsoft.Diagnostics.Monitoring.HostingStartup/ParameterCapturing/FunctionProbes/LogEmittingProbes.cs
+++ src/Microsoft.Diagnostics.Monitoring.HostingStartup/ParameterCapturing/FunctionProbes/LogEmittingProbes.cs
@@ -2,17 +2,36 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using Microsoft.Extensions.Logging;
+using System.Collections.Concurrent;
 using System.Diagnostics;
+using System.Threading;
 
 namespace Microsoft.Diagnostics.Monitoring.HostingStartup.ParameterCapturing.FunctionProbes
 {
     internal sealed class LogEmittingProbes : IFunctionProbes
     {
         private readonly ILogger _logger;
+        private readonly Thread _thread;
+        private BlockingCollection<(string, string[])> _messages;
 
         public LogEmittingProbes(ILogger logger)
         {
             _logger = logger;
+            _thread = new Thread(ThreadProc);
+
+            //Do not schedule ahead of app work
+            _thread.Priority = ThreadPriority.BelowNormal;
+            _messages = new BlockingCollection<(string, string[])>(1024);
+            _thread.Start();
+        }
+
+        private void ThreadProc()
+        {
+            while (true)
+            {
+                var message = _messages.Take();
+                _logger.Log(LogLevel.Information, message.Item1, message.Item2);
+            }
         }
 
         public void EnterProbe(ulong uniquifier, object[] args)
@@ -29,12 +48,16 @@ namespace Microsoft.Diagnostics.Monitoring.HostingStartup.ParameterCapturing.Fun
             }
             */
 
-
             var methodCache = FunctionProbesStub.InstrumentedMethodCache;
             if (methodCache == null ||
                 args == null ||
                 !methodCache.TryGetValue(uniquifier, out InstrumentedMethod? instrumentedMethod) ||
                 args.Length != instrumentedMethod?.SupportedParameters.Length)
+            {
+                return;
+            }
+
+            if (instrumentedMethod.CaptureMode == ParameterCaptureMode.Disallowed)
             {
                 return;
             }
@@ -51,8 +74,17 @@ namespace Microsoft.Diagnostics.Monitoring.HostingStartup.ParameterCapturing.Fun
                 argValues[fmtIndex++] = PrettyPrinter.FormatObject(args[i]);
             }
 
-            // Console.WriteLine("IO Test");
+            if (instrumentedMethod.CaptureMode == ParameterCaptureMode.Background)
+            {
+                _messages.TryAdd((instrumentedMethod.MethodWithParametersTemplateString, argValues));
+                return;
+            }
+
+
+            //
+            // System.Console.WriteLine("IO Test");
             _logger.Log(LogLevel.Information, instrumentedMethod.MethodWithParametersTemplateString, argValues);
+
             return;
         }
     }
--- src/Microsoft.Diagnostics.Monitoring.HostingStartup/ParameterCapturing/InstrumentedMethod.cs
+++ src/Microsoft.Diagnostics.Monitoring.HostingStartup/ParameterCapturing/InstrumentedMethod.cs
@@ -1,16 +1,26 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Collections.Concurrent;
+using System.Linq;
 using System.Reflection;
 
 namespace Microsoft.Diagnostics.Monitoring.HostingStartup.ParameterCapturing
 {
+    public enum ParameterCaptureMode
+    {
+        Inline,
+        Background,
+        Disallowed
+    }
+
     public sealed class InstrumentedMethod
     {
         public InstrumentedMethod(MethodInfo method, uint[] boxingTokens)
         {
             SupportedParameters = BoxingTokens.AreParametersSupported(boxingTokens);
             MethodWithParametersTemplateString = PrettyPrinter.ConstructTemplateStringFromMethod(method, SupportedParameters);
+
             foreach (bool isParameterSupported in SupportedParameters)
             {
                 if (isParameterSupported)
@@ -18,7 +28,44 @@ namespace Microsoft.Diagnostics.Monitoring.HostingStartup.ParameterCapturing
                     NumberOfSupportedParameters++;
                 }
             }
+
+            string[] systemTypes = { "System", "Microsoft" };
+            string[] disallowedTypes = { "Interop+Advapi", "Interop+Kernel32",
+                "System.Collections.Concurrent",
+                "System.ObjectDisposedException",
+                "System.Marvin",
+                "System.Convert",
+                "System.Runtime.InteropServices",
+                "System.Numerics",
+                "System.Threading",
+                "System.Collections.Generic",
+                "System.IO",
+                "System.Runtime",
+                "System.Text",
+                "System.Buffers",
+                "System.Globalization" };
+
+            string[] disallowedNamespaces = { nameof(System) };
+
+            if (disallowedNamespaces.Contains(method.DeclaringType?.Namespace))
+            {
+                CaptureMode = ParameterCaptureMode.Disallowed;
+            }
+            else if (disallowedTypes.Select(type => method.DeclaringType?.FullName?.StartsWith(type) ?? false).Any(t => t))
+            {
+                CaptureMode = ParameterCaptureMode.Disallowed;
+            }
+            else if (systemTypes.Select(type => method.DeclaringType?.FullName?.StartsWith(type) ?? false).Any(t => t))
+            {
+                CaptureMode = ParameterCaptureMode.Background;
+            }
+            else
+            {
+                CaptureMode = ParameterCaptureMode.Inline;
+            }
         }
+
+        public ParameterCaptureMode CaptureMode { get; }
 
         /// <summary>
         /// The total number of parameters (implicit and explicit) that are supported.
